85c52a98e510c59998e2c85e6b3403939f3af521
Adrian <adriandinca2008@yahoo.com>
2019-01-23 22:48:00 +0200

    feat: Purge inlined css styles

--- scripts/purgecss.js
+++ scripts/purgecss.js
@@ -21,7 +21,6 @@ RegExp.escape = function (string) {
 }
 
 const extractorPattern = /[A-Za-z0-9-_\/]+/g;
-const jsExtractor = /[A-Za-z0-9-_:\/]+/g
 
 class HtmlBodyExtractor {
   static extract(content) {
@@ -39,15 +38,6 @@ class DefaultExtractor {
   }
 }
 
-class JsExtractor {
-  static extract(content) {
-    const res = content.match(extractorPattern);
-    console.error('extracted selectors:');
-    console.error(res);
-    return res || [];
-  }
-}
-
 const purgeCss = new PurgeCss({
   extractors: [{
     extractor: HtmlBodyExtractor, // Only purge the body so we do not take into account the selectors from the inlined styles (which are not yet purged)
@@ -58,19 +48,17 @@ const purgeCss = new PurgeCss({
   }],
   content: [
     path.join(PATHS.public, '/**/*.html'),
-    path.join(PATHS.node_modules, '/@material/!(react-*)/**/!(*.d).{ts,js,jsx,tsx}'),
+    path.join(PATHS.node_modules, '/@material/!(react-*)/**/!(*.d).{js,jsx,tsx,tsx}'),
   ],
   css: [path.join(PATHS.public, '/**/*.css')],
   // whitelistPatterns: [/-upgraded/, /-ripple/], // Keep selectors that match this patterns
   // TODO: Check purgecss' keyframes purging algorithm, since it does not seem to work for multiple animations
   keyframes: true, // Remove unused keyframes
   fontFace: true, // Remove unsued @font-face rules
-  rejected: true,
+  // rejected: true,
 });
 
 const purgeResult = purgeCss.purge();
-
-// console.log(purgeResult);
 
 (async () => {
   // Write the resulted css in each css file
@@ -80,21 +68,17 @@ const purgeResult = purgeCss.purge();
   glob(`${PATHS.public}/**/*.html`, (err, files) =>
     files.forEach(async file => {
       let html = await readFile(file, 'utf8');
-      const inlinedStylePattern = /<style data-href="\/([\s\S]*?)">([\s\S]*?)<\/style>/g;
+      const inlinedStylePattern = /<style data-href="\/.*?">.*?<\/style>/gm;
 
-      let styleTagMatches = inlinedStylePattern.exec(html);
-      while (styleTagMatches) {
-        const cssFileName = styleTagMatches[1];
-        const inlinedStyles = styleTagMatches[2];
+      const styleTags = html.match(inlinedStylePattern);
+      styleTags.forEach(styleTag => {
+        const [, cssFile, inlinedStyles] = styleTag.match(/<style data-href="\/(.*?)">(.*?)<\/style>/m);
 
         for (let i = 0; i < purgeResult.length; i++) {
-          if (path.basename(purgeResult[i].file) === cssFileName) {
-            console.log(cssFileName);
-            console.log(purgeResult[i].css);
-
+          if (path.basename(purgeResult[i].file) === cssFile) {
             const purgeCss = new PurgeCss({
               extractors: [{
-                extractor: JsExtractor,
+                extractor: DefaultExtractor,
                 extensions: ['css'],
               }],
               content: [{
@@ -105,83 +89,15 @@ const purgeResult = purgeCss.purge();
               rejected: true,
             });
 
-            const pRes = purgeCss.purge();
+            const purgedInlinedStyles = purgeCss.purge()[0].css;
+            html = html.replace(inlinedStyles, purgedInlinedStyles);
 
-            console.log(pRes);
-          }
-          if (path.basename(purgeResult[i].file) === cssFileName) {
-            // const purgeCss = new PurgeCss({
-            //   extractors: [{
-            //     extractor: JsExtractor,
-            //     extensions: ['css'],
-            //   }],
-            //   content: [{
-            //     raw: purgeResult[i].css,
-            //     extension: 'css'
-            //   }],
-            //   css: [{ raw: inlinedStyles }],
-            //   rejected: true,
-            // });
-
-            // const pRes = purgeCss.purge();
-
-            // console.log(pRes);
-
-            // const purgedInlinedStyles = pRes[0].css;
-
-            // html = html.replace(inlinedStyles, purgedInlinedStyles);
-
-            // break;
+            break;
           }
         }
-
-        styleTagMatches = inlinedStylePattern.exec(html);
-      }
-
-      return;
-
-      // Purge the css inside each inlined style tag
-      purgeResult.forEach(key => {
-        // console.log(key);
-        const cssFileName = path.basename(key.file);
-        const styleTagMatches = html.match(new RegExp(`<style data-href="/${RegExp.escape(cssFileName)}">([\\s\\S]*?)</style>`));
-
-        if (!styleTagMatches) { return; }
-
-        const inlinedStyles = styleTagMatches[1];
-
-        if (!inlinedStyles) { return; }
-
-        // Since the css files have already been purged, we can use them to only keep the needed selectors
-        const purgeCss = new PurgeCss({
-          extractors: [{
-            extractor: JsExtractor,
-            extensions: ['css'],
-          }],
-          content: [{
-            raw: key.css,
-            extension: 'css'
-          }],
-          css: [{ raw: inlinedStyles }],
-          rejected: true
-        });
-
-        const pRes = purgeCss.purge();
-
-        // if (cssFileName.startsWith('11')) {
-        // console.log(pRes);
-        // }
-
-        const purgedInlinedStyles = pRes[0].css;
-        // if (!pRes[0].css.startsWith('html')) {
-        //   console.log(pRes[0].css, pRes[0].rejected);
-        // }
-
-        html = html.replace(inlinedStyles, purgedInlinedStyles);
       });
 
-      // Update the file
       writeFile(file, html, 'utf8');
     })
   );
-})()
+})();
